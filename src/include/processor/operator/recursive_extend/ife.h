103dd247e6ad597dc163295a692299646201c8fd
xiyang <x74feng@uwaterloo.ca>
2024-03-20 22:37:45 -0400

    X

--- src/include/processor/operator/recursive_extend/ife.h
+++ src/include/processor/operator/recursive_extend/ife.h
@@ -7,20 +7,41 @@ namespace kuzu {
 namespace processor {
 
 struct TargetNodes {};
+struct FrontierMorsel;
+
 struct IFEState {
+    std::mutex mtx;
+
     common::nodeID_t startNode;
 
+    uint8_t currentLevel;
 
+    uint32_t numThreadsRegistered;
+    uint32_t numThreadsFinished;
+
+    void registerThread();
+    void finishThread();
+
+    bool completed();
+    bool currentLevelCompleted();
+    void finializeLevel(); // Be careful! This will have a race condition.
+    FrontierMorsel getMorsel();
 };
 
-struct IFEMorsel {
+struct FrontierMorsel {
+    std::mutex mtx;
+    // Frontier should be retrivable from
     IFEState* state;
+    uint8_t level;
     // Range from current frontier.
     common::offset_t startOffset;
     common::offset_t endOffset;
+
+    bool empty() const { return endOffset == startOffset; }
 };
 
 struct IFESharedState {
+    std::mutex mtx;
     // Input tuples.
     std::shared_ptr<FactorizedTable> inputTable;
     common::offset_t tableCursor;
@@ -30,15 +51,36 @@ struct IFESharedState {
     // TODO: we probably don't need a dynamic data structure.
     std::vector<std::shared_ptr<IFEState>> activeIFEState;
 
-    IFEMorsel getMorsel()  {
+    IFEState* getIFEState()  {
+        std::unique_lock lck{mtx};
         if (tableCursor < inputTable->getNumTuples()) {
-            // Scan from inputTable and generate new IFE.
-            common::nodeID_t startNode;
-            auto state = std::make_shared<IFEState>();
-            tableCursor++;
-            return IFEMorsel();
+            activeIFEState.push_back({});
+            // Generate new IFE state and return
         }
-        // Scan from active IFE state.
+        // Return an active IFE state.
+        return activeIFEState[0].get();
+    }
+};
+
+//struct Node {
+//    common::nodeID_t nodeID;
+//    Node* next;
+//};
+
+struct Frontier {
+    // I need hash map implementation that allows me to go from a node_ID to an object.
+    // This object
+};
+
+struct DGraph {
+
+    std::vector<Frontier> frontiers;
+    Frontier* current;
+    Frontier* next;
+
+    // Assuming
+    void insert(common::nodeID_t u, common::relID_t r, common::nodeID_t v) {
+
     }
 };
 
@@ -46,21 +88,39 @@ struct IFESharedState {
 class IFE : public PhysicalOperator {
 public:
     bool getNextTuplesInternal(ExecutionContext *context) override {
-        // try get a new IFE state
-        // fail
+        while (true) {
+            // if can scan from graph
+            //      return true
+            auto ifeState = sharedState->getIFEState();
+            if (ifeState == nullptr) { // No more ife to compute
+                return false;
+            }
+            ifeState->registerThread(); // track how many threads
+            computeIFE(ifeState);
+        }
+    }
+
+    bool computeIFE(IFEState* state) {
+        while (!state->completed()) {
+            auto frontierMorsel = state->getMorsel();
+            if (frontierMorsel.empty()) { // no more work on current frontier.
+                state->finishThread(); // notify thread is done with current frontier.
+                if (!state->currentLevelCompleted()) {
+                    // thread sleep. wait for other thread to finish
+                } else {
+                    state->finializeLevel();
+                }
+                continue;
+            }
+            // perform thread local scan and extend
+            // update thread local DGraph
+        }
     }
 
 
 private:
     std::shared_ptr<IFESharedState> sharedState;
 };
-
-struct IFEScan {
-    virtual void scan(const IFEMorsel& morsel) {
-
-    }
-};
-
 
 }
 }
